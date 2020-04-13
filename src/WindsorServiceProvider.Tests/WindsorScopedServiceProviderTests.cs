034ccc3e11063acbf274f31c7818ed0ec1578b4c
Lukas Tines <ltines@euroffice.co.uk>
2020-04-13 22:21:53 +0100

    Remove copied tests

--- src/WindsorServiceProvider.Tests/WindsorScopedServiceProviderTests.cs
+++ src/WindsorServiceProvider.Tests/WindsorScopedServiceProviderTests.cs
@@ -14,27 +14,5 @@ namespace WindsorServiceProvider.Tests
             var container = factory.CreateBuilder(serviceCollection);
             return factory.CreateServiceProvider(container);
         }
-
-        [Fact]
-        public void NestedScopedServiceCanBeResolved2()
-        {
-            // Arrange
-            var collection = new TestServiceCollection();
-            collection.AddScoped<IFakeScopedService, FakeService>();
-            var provider = CreateServiceProvider(collection);
-
-            // Act
-            using (var outerScope = provider.CreateScope())
-            using (var innerScope = outerScope.ServiceProvider.CreateScope())
-            {
-                var outerScopedService = outerScope.ServiceProvider.GetService<IFakeScopedService>();
-                var innerScopedService = innerScope.ServiceProvider.GetService<IFakeScopedService>();
-
-                // Assert
-                Assert.NotNull(outerScopedService);
-                Assert.NotNull(innerScopedService);
-                Assert.NotSame(outerScopedService, innerScopedService);
-            }
-        }
     }
 }
